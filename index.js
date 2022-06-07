25cae062de1b4b2c0caa855e645b7b77be188d57
margaret manyeki <maggiemanyeki6@gmail.com>
2022-06-07 16:10:40 +0300

    commit tree 25a943b99a2e62559f91cc2a6d04425add4343a3

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+if(feet <= 400){
+  return "This one is on me!"
+}
+else if (feet>2000 && feet <2501 ){
+  return 'I will gladly take your thirty bucks.';
+}
+else if (feet>2500){
+  return 'No can do.';
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+    return city === 'NYC' ? "Ok, sounds good." :"No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case 'generous':
+    return 'Thank you so much.';
+    break;
+    case 'not as generous':
+    return 'Thank you.';
+    break;
+
+    case 'thanks for everything':
+      return 'Bye.';
+  }
 }
